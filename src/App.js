61374c8e02b5707b9455f2d377edfa8997983f5e
beyzagoknl <beyzagoknl@gmail.com>
2022-09-18 21:42:21 +0200

    created project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Tweet from "./Tweet";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://coursera-twitter-api.herokuapp.com/tweets"
+      );
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("here");
+    fetchData();
+  });
+
+  return (
+    <div style={{ background: "#111111", paddiing: "30px" }}>
+      {data.map((tweet) => (
+        <Tweet key={tweet.id} {...tweet} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
